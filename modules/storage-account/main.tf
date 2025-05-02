1a2404eb1d449d1599953f9018740175d5d76775
nash-cloud <shahjalal.codex@gmail.com>
2025-05-02 16:43:11 -0500

    modified storage-account variables

--- modules/storage-account/main.tf
+++ modules/storage-account/main.tf
@@ -2,10 +2,6 @@
 
 data "azurerm_client_config" "current" {}
 
-data "http" "ip" {
-  url = "https://ifconfig.me"
-}
-
 resource "azurerm_storage_account" "adl_st" {
   name                          = "st${var.basename}"
   resource_group_name           = var.resource_group_name
@@ -16,6 +12,7 @@ resource "azurerm_storage_account" "adl_st" {
   is_hns_enabled                = var.hns_enabled
   min_tls_version               = var.min_tls_version
   public_network_access_enabled = var.public_network_access_enabled
+  
   queue_properties {
     logging {
       delete                = true
@@ -63,54 +60,6 @@ resource "azurerm_storage_account_network_rules" "firewall_rules" {
   ip_rules                   = var.firewall_ip_rules
   virtual_network_subnet_ids = var.firewall_virtual_network_subnet_ids
   bypass                     = var.firewall_bypass
-
-  count = var.module_enabled ? 1 : 0
-}
-
-module "st_pe_blob" {
-  source                         = "../private-endpoint"
-  basename                       = "${azurerm_storage_account.adl_st[0].name}-blob"
-  resource_group_name            = var.resource_group_name
-  location                       = var.location
-  subnet_id                      = var.subnet_id
-  private_connection_resource_id = azurerm_storage_account.adl_st[0].id
-  subresource_names              = ["blob"]
-  is_manual_connection           = false
-  private_dns_zone_ids           = var.private_dns_zone_ids_blob
-  tags                           = var.tags
-  module_enabled                 = var.module_enabled && var.is_private_endpoint && length(var.private_dns_zone_ids_blob) != 0
-
-  count = var.module_enabled ? 1 : 0
-}
-
-module "st_pe_file" {
-  source                         = "../private-endpoint"
-  basename                       = "${azurerm_storage_account.adl_st[0].name}-file"
-  resource_group_name            = var.resource_group_name
-  location                       = var.location
-  subnet_id                      = var.subnet_id
-  private_connection_resource_id = azurerm_storage_account.adl_st[0].id
-  subresource_names              = ["file"]
-  is_manual_connection           = false
-  private_dns_zone_ids           = var.private_dns_zone_ids_file
-  tags                           = var.tags
-  module_enabled                 = var.module_enabled && var.is_private_endpoint && length(var.private_dns_zone_ids_file) != 0
-
-  count = var.module_enabled ? 1 : 0
-}
-
-module "st_pe_dfs" {
-  source                         = "../private-endpoint"
-  basename                       = "${azurerm_storage_account.adl_st[0].name}-dfs"
-  resource_group_name            = var.resource_group_name
-  location                       = var.location
-  subnet_id                      = var.subnet_id
-  private_connection_resource_id = azurerm_storage_account.adl_st[0].id
-  subresource_names              = ["dfs"]
-  is_manual_connection           = false
-  private_dns_zone_ids           = var.private_dns_zone_ids_dfs
-  tags                           = var.tags
-  module_enabled                 = var.module_enabled && var.is_private_endpoint && length(var.private_dns_zone_ids_dfs) != 0
 
   count = var.module_enabled ? 1 : 0
 }
--- modules/storage-account/variables.tf
+++ modules/storage-account/variables.tf
@@ -21,33 +21,9 @@ variable "location" {
   description = "Location of the resource group."
 }
 
-variable "tags" {
-  type        = map(string)
-  description = "A mapping of tags which should be assigned to the Resource Group."
-  default     = {}
-}
-
-variable "module_enabled" {
-  type        = bool
-  description = "Variable to enable or disable the module."
-  default     = true
-}
-
-variable "is_private_endpoint" {
-  type        = bool
-  description = "Whether private endpoints are enabled to access the resource."
-  default     = true
-}
-
-variable "subnet_id" {
-  type        = string
-  description = "The ID of the subnet from which private IP addresses will be allocated for this Private Endpoint."
-  default     = ""
-}
-
 variable "account_tier" {
   type        = string
-  description = "Defines the Tier to use for this storage account."
+  description = "Defines the Tier to use for this storage account. R"
   validation {
     condition     = contains(["standard", "premium"], lower(var.account_tier))
     error_message = "Supported values for account_tier are \"standard\" or \"premium\"."
@@ -57,7 +33,7 @@ variable "account_tier" {
 
 variable "account_replication_type" {
   type        = string
-  description = "Defines the type of replication to use for this storage account."
+  description = "Defines the type of replication to use for this storage account. R"
   validation {
     condition     = contains(["lrs", "grs", "ragrs", "zrs", "gzrs", "ragzrs"], lower(var.account_replication_type))
     error_message = "Supported values for account_replication_type are \"LRS\", \"GRS\", \"RAGRS\", \"ZRS\", \"GZRS\", \"RAGZRS\"."
@@ -67,7 +43,7 @@ variable "account_replication_type" {
 
 variable "account_kind" {
   type        = string
-  description = "Defines the Kind of account."
+  description = "Defines the Kind of account. R"
   validation {
     condition     = contains(["blobstorage", "blockblobstorage", "filestorage", "storage", "storagev2"], lower(var.account_kind))
     error_message = "Supported values for account_kind are \"BlobStorage\", \"BlockBlobStorage\", \"FileStorage\", \"Storage\", or \"StorageV2\"."
@@ -81,15 +57,38 @@ variable "hns_enabled" {
   default     = true
 }
 
-variable "firewall_virtual_network_subnet_ids" {
-  type        = list(string)
-  description = "A list of virtual network subnet ids to to secure the storage account."
-  default     = []
+variable "min_tls_version" {
+  type        = string
+  description = "The minimum supported TLS version for the storage account. optional"
+  validation {
+    condition     = contains(["tls1_0", "tls1_1", "tls1_2"], lower(var.min_tls_version))
+    error_message = "Valid values for min_tls_version are \"TLS1_0\", \"TLS1_1\", or \"TLS1_2\"."
+  }
+  default = "TLS1_2"
 }
 
+variable "public_network_access_enabled" {
+  type        = bool
+  description = "Whether or not public network access is allowed for this resource."
+  default     = true
+}
+
+variable "tags" {
+  type        = map(string)
+  description = "A mapping of tags which should be assigned to the Resource Group. Optional"
+  default     = {}
+}
+
+variable "module_enabled" {
+  type        = bool
+  description = "Variable to enable or disable the module. Optional"
+  default     = true
+}
+
+# "network_rules" block variables
 variable "firewall_default_action" {
   type        = string
-  description = "Specifies the default action of allow or deny when no other rules match."
+  description = "Specifies the default action of allow or deny when no other rules match. Required"
   validation {
     condition     = contains(["allow", "deny"], lower(var.firewall_default_action))
     error_message = "Valid values for firewall_default_action are \"Allow\" or \"Deny\"."
@@ -99,50 +98,22 @@ variable "firewall_default_action" {
 
 variable "firewall_ip_rules" {
   type        = list(string)
-  description = "List of public IP or IP ranges in CIDR Format."
+  description = "List of public IP or IP ranges in CIDR Format. optional"
   default     = []
+}
+
+variable "firewall_virtual_network_subnet_ids" {
+    type = list(string)
+    description = "A list of virtual network subnet ids to secure the storage account. optional"
+    default = []
 }
 
 variable "firewall_bypass" {
   type        = list(string)
-  description = "Specifies whether traffic is bypassed for Logging/Metrics/AzureServices."
+  description = "Specifies whether traffic is bypassed for Logging/Metrics/AzureServices. optional"
   validation {
     condition     = alltrue([for v in var.firewall_bypass : contains(["logging", "metrics", "azureservices", "none"], lower(v))])
     error_message = "Valid values for firewall_bypass are \"Logging\", \"Metrics\", \"AzureServices\" or \"None\"."
   }
   default = ["AzureServices"]
-}
-
-variable "min_tls_version" {
-  type        = string
-  description = "The minimum supported TLS version for the storage account."
-  validation {
-    condition     = contains(["tls1_0", "tls1_1", "tls1_2"], lower(var.min_tls_version))
-    error_message = "Valid values for min_tls_version are \"TLS1_0\", \"TLS1_1\", or \"TLS1_2\"."
-  }
-  default = "TLS1_2"
-}
-
-variable "private_dns_zone_ids_blob" {
-  type        = list(string)
-  description = "Specifies the list of Private DNS Zones to include."
-  default     = []
-}
-
-variable "private_dns_zone_ids_file" {
-  type        = list(string)
-  description = "Specifies the list of Private DNS Zones to include."
-  default     = []
-}
-
-variable "private_dns_zone_ids_dfs" {
-  type        = list(string)
-  description = "Specifies the list of Private DNS Zones to include."
-  default     = []
-}
-
-variable "public_network_access_enabled" {
-  type        = bool
-  description = "Whether or not public network access is allowed for this resource."
-  default     = true
 }
