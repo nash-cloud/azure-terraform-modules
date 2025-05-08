08a055ca8522afe33bf4417385cf94cae1f44f01
nash-cloud <shahjalal.codex@gmail.com>
2025-05-08 11:46:06 -0500

    added remote backend for terraform state

--- test/providers.tf
+++ test/providers.tf
@@ -1,10 +1,10 @@
 terraform {
-  # backend "azurerm" {
-  #   resource_group_name  = "rg-adl-terraform-state"
-  #   storage_account_name = "stadlterraformstate"
-  #   container_name       = "default"
-  #   key                  = "datafactory.terraform.tfstate"
-  # }
+  backend "azurerm" {
+    resource_group_name  = "rg-tfstate"
+    storage_account_name = "storagetfstateacct"
+    container_name       = "default"
+    key                  = "datalab.terraform.tfstate"
+  }
 
   required_providers {
     azurerm = {
